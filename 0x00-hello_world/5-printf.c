b2beb516f5d8e4bcd4687ced06578b99359b2744
tavolafourcade <tavolafourcade@gmail.com>
2021-02-04 23:47:09 +0000

    Improved the last version

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* main - Use of printf
+* main - Prints a message
 *
-* Description: prints exactly with proper grammar, 
-* ( but the outcome is a piece of art,, followed by a new line.
-* Return: 0 if it is successful
+* Return:  0 if it is successful
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
