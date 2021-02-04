cde3bf24a3ad4c4e1bbf588c0b781822786e3f4d
tavolafourcade <tavolafourcade@gmail.com>
2021-02-04 22:56:07 +0000

    Tried some changes related to indentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,12 @@
 /**
 * main - Use of printf
 *
-* Description: prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+* Description: prints exactly with proper grammar, 
+( but the outcome is a piece of art,, followed by a new line.
 * Return: 0 if it is successful
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
