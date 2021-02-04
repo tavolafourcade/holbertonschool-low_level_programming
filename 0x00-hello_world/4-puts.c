32c933ea15ce8d019d9203f19afcc8de00adea21
tavolafourcade <tavolafourcade@gmail.com>
2021-02-04 19:10:54 +0000

    Added comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+* main - Use puts to print a string
+*
+* Description: This function will print the string "Programming is like
+ *building a multilingual puzzle using the puts function.
+* Return: 0 if successful
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
