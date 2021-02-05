3c4c830a81e5f2c0d643fce6d1430d31385457bf
tavolafourcade <tavolafourcade@gmail.com>
2021-02-05 04:05:45 +0000

    Fixed general bugs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
+	else if (n == 00)
+		printf("%d is zero\n", n);
 	else if (n < 0)
-		printf("%d is negative", n);
-	else
-		printf("%d is zero", n);
-	putchar("\n");
+		printf("%d is negative\n", n);
 	return (0);
-}
+	}
