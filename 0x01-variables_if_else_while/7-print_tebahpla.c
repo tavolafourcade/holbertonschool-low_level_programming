b3d1a86672a20880c7fc672e4ca5aa20dbab8d30
tavolafourcade <tavolafourcade@gmail.com>
2021-02-05 06:04:31 +0000

    Fixed bugs

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,14 +4,14 @@
 /**
  * main - Print the lowercase alphabet in reverse
  *
- * Return:0 
+ * Return: 0 
  */
 
 int main(void)
 {
 char letter;
 for (letter = 'z'; letter >= 'a'; letter--)
-	putchar(letter);
-	putchar('\n');
-	return(0);
+putchar(letter);
+putchar('\n');
+return(0);
 }
