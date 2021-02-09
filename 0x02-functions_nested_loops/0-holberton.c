53fc2094389c5e43545bc9976dea937148455845
tavolafourcade <tavolafourcade@gmail.com>
2021-02-09 15:04:59 +0000

    Created header holberton.h and imported the function declaration into 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "holberton.h"
 
+/**
+  * main - Print Holberton using _putchar
+  *
+  * Return: 0
+  */
 int main(void)
 {
-	printf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
 	return (0);
 }
