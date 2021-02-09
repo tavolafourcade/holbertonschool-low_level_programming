bd84ea7bc717ece5fe392eb8103beac5495c052b
tavolafourcade <tavolafourcade@gmail.com>
2021-02-09 17:22:17 +0000

    Fixed Bettys issues

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,16 @@
   * main - Prints the alphabet
   *
   * Return: 0
-  */
+*/
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter < 'z'; letter++)
 	{
 		_putchar(letter);
 	}
+
 	_putchar('\n');
 }
 
