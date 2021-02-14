1907f92d9b2551b86a3631067a9eb0f65e2ddbd6
tavolafourcade <tavolafourcade@gmail.com>
2021-02-14 22:39:28 +0000

    Write a function that checks for uppercase character.
    Prototype: int _isupper(int c);
    Returns 1 if c is uppercase
    Returns 0 otherwise

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _isupper - It checks if c is uppercase or not
+ * @c: variable to be evaluated
+ * Return: 1 if c iS uppercase 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c > 'A' && c <= 'Z')
+{
+	return (1);
+}
+return (0);
+}
