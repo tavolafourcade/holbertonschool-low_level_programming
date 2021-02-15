00096ec823cd2447ad1b0026f96b9eb9ecdf8df0
tavolafourcade <tavolafourcade@gmail.com>
2021-02-15 02:17:07 +0000

    Write a function that prints a square, followed by a new line.
    Prototype: void print_square(int size);
    You can only use _putchar function to print
    Where size is the size of the square
    If size is 0 or less, the function should print only a new line
    Use the character # to print the square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *print_square - It will print a square of #
+ *@size: Length of the square
+ *Return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
