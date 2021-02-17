d345f8730582755edf9972f6ab6e201e3dfc0783
tavolafourcade <tavolafourcade@gmail.com>
2021-02-17 17:54:44 +0000

    Impoved indentation and code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  *print_rev - function that prints a string in reverse
  *@s: string
@@ -6,14 +7,15 @@
  */
 void print_rev(char *s)
 {
-int i = 0;
+	int i = 0;
 
-while (s[i] != '\0')
-	i++;
-while (i >= 0)
-{
-_putchar(s[i]);
-i--;
-}
-_putchar('\n');
+	while (s[i] != '\0')
+		i++;
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
 }
