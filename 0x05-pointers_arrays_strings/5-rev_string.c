807af28f50225d0e6b7ed6a2c4d384c96c03c49f
tavolafourcade <tavolafourcade@gmail.com>
2021-02-18 01:53:48 +0000

    Write a function that reverses a string.
    Prototype: void rev_string(char *s);

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ *rev_string - function that reverses a string
+ *@s: string
+ *Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char length = _strlen(s);
+	char temporal;
+
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+		temporal = s[i];
+		s[i] = s[j];
+		s[j] = temporal;
+	}
+}
+/**
+*_strlen - returns the total length of a string
+*@c: array
+*Return: the total length
+*/
+int _strlen(char *c)
+{
+int i;
+int length = 0;
+
+for (i = 0; c[i] != '\0'; ++i)
+	length++;
+return (length);
+}
